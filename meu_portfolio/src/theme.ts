ae2882d5016a5ab398f87b41db8ac18f6c236572
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2024-10-17 21:45:32 -0300

    feat: configs material ui theme, css baseline

---
+++ meu_portfolio/src/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+    secondary: {
+      main: '#4f8e3e',
+    },
+  },
+  typography:{
+    fontFamily: "Roboto"
+  }
+});
+
+theme = responsiveFontSizes(theme)
+
+export default theme;
